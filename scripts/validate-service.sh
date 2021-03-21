ae9dc5650cf29e5c1fdcb3ae0468c5cb8dbf9729
subbu-devasani <74688372+subbu-devasani@users.noreply.github.com>
2021-03-21 21:18:18 +0530

    Update validate-service.sh

--- scripts/validate-service.sh
+++ scripts/validate-service.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-cd /home/ubuntu/Travello_web_app
-sudo service nginx restart
 
 echo "The ValidateService deployment lifecycle event successfully completed." > validate-service.txt
 
-unset FOLDER
