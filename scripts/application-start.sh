0fae2e2f2debf6adbc3e9b5b2b0785d0e724dde3
subbu-devasani <74688372+subbu-devasani@users.noreply.github.com>
2021-03-21 20:42:54 +0530

    Update application-start.sh

--- scripts/application-start.sh
+++ scripts/application-start.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 cd /home/ubuntu/Travello_web_app
 
-python3 manage.py collectstatic
-
-sudo systemctl daemon-reload
-
-sudo systemctl start gunicorn
-sudo systemctl enable gunicorn
 
 sudo service nginx restart
 
