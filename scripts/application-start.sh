aea4a51d79077fded1e65d52ce5430fa3ea68e36
subbu-devasani <74688372+subbu-devasani@users.noreply.github.com>
2021-03-21 20:25:10 +0530

    Update application-start.sh

--- scripts/application-start.sh
+++ scripts/application-start.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 cd /home/ubuntu/Travello_web_app
 
+python3 manage.py collectstatic
+
+sudo systemctl daemon-reload
+
+sudo systemctl start gunicorn
+sudo systemctl enable gunicorn
+
+sudo service nginx restart
+
 echo "The ApplicationStart deployment lifecycle event successfully completed." > application-start.txt
