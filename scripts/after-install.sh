1580d64b04ad19e5ab0d2dabae35b64b19ec2357
subbu-devasani <74688372+subbu-devasani@users.noreply.github.com>
2021-03-21 20:43:02 +0530

    Update after-install.sh

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 cd /home/ubuntu/Travello_web_app
 
+python3 manage.py collectstatic
+
+sudo systemctl daemon-reload
+
+sudo systemctl start gunicorn
+sudo systemctl enable gunicorn
+
+sudo service nginx restart
+
 echo "The AfterInstall deployment lifecycle event successfully completed." > after-install.txt
