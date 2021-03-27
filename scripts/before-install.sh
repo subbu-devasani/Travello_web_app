6aa6be97fa8477a03490c345e9f243c76ae653e4
subbu-devasani <74688372+subbu-devasani@users.noreply.github.com>
2021-03-28 01:29:31 +0530

    Update before-install.sh

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-##export FOLDER=/home/ubuntu/
+export FOLDER=/home/ubuntu/
 
-#if [ -d $FOLDER ]
-#then
-#rm -rf $FOLDER
+if [ -d $FOLDER ]
+then
+ rm -rf $FOLDER
 fi
 
-#mkdir -p $FOLDER
-
-echo "The before-install deployment lifecycle event successfully completed." > before-install.sh.txt
+mkdir -p $FOLDER
