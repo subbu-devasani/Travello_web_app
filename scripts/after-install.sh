c5a5b21cda198495f5ed7b1314d292fa4b8bff79
subbu-devasani <74688372+subbu-devasani@users.noreply.github.com>
2021-03-28 00:48:10 +0530

    Update after-install.sh

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-
+cd /home/ubuntu/Travello_web_app
+sudo virtualenv venv
+source venv/bin/activate
+sudo apt-get install libpq-dev
+pip3 install -r requirments.txt
+python3 manage.py makemigrations
+python3 manage.py migrate
+python3 manage.py collectstatic
+cd ..
 
 echo "The AfterInstall deployment lifecycle event successfully completed." > after-install.txt
