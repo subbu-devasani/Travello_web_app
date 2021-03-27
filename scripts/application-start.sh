dbb7b27e36a2e2efc05891d6e7dd3de0e06e3113
subbu-devasani <74688372+subbu-devasani@users.noreply.github.com>
2021-03-28 00:50:28 +0530

    Update application-start.sh

--- scripts/application-start.sh
+++ scripts/application-start.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+sudo systemctl daemon-reload
+sudo systemctl start gunicorn
+sudo systemctl enable gunicorn
+sudo service gunicorn restart
+sudo service nginx restart 
+
 echo "The ApplicationStart deployment lifecycle event successfully completed." > application-start.txt
