3e14c3640fb97a50c79a749cb0fb1cf148134b53
subbu-devasani <74688372+subbu-devasani@users.noreply.github.com>
2021-03-28 08:12:31 +0530

    Update after-install.sh

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -1,5 +1,12 @@
-  
 #!/bin/bash
+
 cd /home/ubuntu/Travello_web_app
+sudo virtualenv venv
+source venv/bin/activate
+cd Travello_web_app
+pip3 install -r requirments.txt
+python3 manage.py makemigrations
+python3 manage.py migrate
+cd ..
 
 echo "The AfterInstall deployment lifecycle event successfully completed." > after-install.txt
